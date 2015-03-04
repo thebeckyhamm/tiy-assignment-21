572f26fc62d3ca732d5a8ec92e36da6124f46096
Becky Hamm <becky@metricslice.com>
2015-03-04 13:47:21 -0500

    reorganized to use collection

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 var Employee = Backbone.Model.extend({});
 
+var Employees = Backbone.Collection.extend({
+    url: "js/data.json",
+    model: Employee
+});
+
+
 var Heading = (function() {
 
     var template = JST["th"];
@@ -33,6 +39,7 @@ var Heading = (function() {
     return Heading;
 })();
 
+
 var Row = (function(){
 
     var template = JST["tr"];
@@ -53,37 +60,27 @@ var Row = (function(){
 })();
 
 
+var employees = new Employees();
+
+
 $(function(){
 
-    $.ajax("js/data.json").done(function(data) {
-        var models = [];
-        var allKeys = [];
-        var uniqKeys;
+    employees.on("add", function(employee) {
+        var row = new Row(employee);
+        row.render();
+    });
 
+    employees.fetch().done(function() {
+        var allKeys = [], uniqKeys;
 
-        // make new employees, add them to the model,
-        // and render each row per employee
-        _.each(data, function(datum) {
-            var employee = new Employee(datum);
-            models.push(employee);
-
-            var row = new Row(employee);
-            row.render();
-        });
-
-
-        // push all keys to an array,
-        // flatten and uniq it,
-        _.each(models, function(employee) {
+        employees.each(function(employee) {
             allKeys.push(employee.keys());
         });
+
         uniqKeys = _.uniq(_.flatten(allKeys));
-        
-        // then render the heading with
-        // the uniq keys
+
         var heading = new Heading(uniqKeys);
         heading.render();
-
-        
     });
+
 });
