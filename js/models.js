34fd4a988788d82b40991e92714b0908a962a7df
Becky Hamm <becky@metricslice.com>
2015-03-10 19:31:13 -0400

    add comments to models

--- js/models.js
+++ js/models.js
@@ -1,5 +1,8 @@
+
+// individual employee
 var EmployeeModel = Backbone.Model.extend({});
 
+// pass in employee model and url
 var EmployeesCollection = Backbone.Collection.extend({
     url: "js/data.json",
     model: EmployeeModel
