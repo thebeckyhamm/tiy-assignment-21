db7a891c09c9d729d9d028652fe70f2927da802f
Becky Hamm <becky@metricslice.com>
2015-03-10 19:29:38 -0400

    remove unneeded models

--- js/models.js
+++ js/models.js
@@ -4,9 +4,3 @@ var EmployeesCollection = Backbone.Collection.extend({
     url: "js/data.json",
     model: EmployeeModel
 });
-
-var DeptModel = Backbone.Model.extend({});
-
-var DepartmentsCollection = Backbone.Collection.extend({
-    model: DeptModel
-});
